b2b08ce1710b42e72fb994ab908577d0f3649a9f
Solon Guedes <104541996+solonguedes@users.noreply.github.com>
2026-05-08 13:25:16 +0000

    add home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1> Welcome to Zadus!! </h1>
+    return (
+        <>
+            <h1> Welcome to Zadus!! </h1>
+            <div> <p> Estou apenas testando e brincando com a linguagem e deploys  </p></div>
+        </>
+    )
 }
 
 export default Home;
